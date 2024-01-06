b0a423d69af0a2b121305ecb29b6431bead81f57
abushoaib99 <abushoaib99@gmail.com>
2024-01-06 10:42:52 +0600

    - Not Found page design done

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,6 +1,19 @@
+// Import CSS
+import "./NotFound.css";
+
+// Import React Link
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <section className="not-found">
+      <div className="container not-found-container">
+        <h1>404</h1>
+        <h3>Opps, This Page Not Found</h3>
+        <Link to={"/"} className="btn btn-border">
+          Go To Home
+        </Link>
+      </div>
+    </section>
   );
 }
