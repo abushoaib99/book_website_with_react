819d0f81fdc7efc09259c858a74db469afd4c914
abushoaib99 <abushoaib99@gmail.com>
2024-01-12 19:10:40 +0600

    - remove scroll when component unmount

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,77 +1,86 @@
 // Import Css
-import './Nav.css';
+import "./Nav.css";
 
 // Import Router Link
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 
 // Import Logo
-import Logo from '../../assets/logo.png';
+import Logo from "../../assets/logo.png";
 
 // Import NavData
-import { navLinks, navRight } from '../../Data/Data'
+import { navLinks, navRight } from "../../Data/Data";
 
 // Import Icons
 import { VscMenu } from "react-icons/vsc";
 import { GrClose } from "react-icons/gr";
 
 // Import Hokes
-import {useState} from 'react';
-
+import { useState, useEffect } from "react";
 
 export default function Nav() {
   // useState for NavLinks show and hide
   const [isNavLinkShow, setIsNavLinkShow] = useState(false);
 
-  // Window scroll NavLinks effects
-  window.addEventListener('scroll', ()=>{
+  const handleScroll = () => {
     setIsNavLinkShow(false);
-  })
+  };
+
+  useEffect(() => {
+    // Window scroll NavLinks effects
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <nav>
       <div className="container nav-container">
         {/* ............Logo............*/}
-        <Link className="logo" to={'/'}>
-          <img src={Logo} alt="Logo"/>
+        <Link className="logo" to={"/"}>
+          <img src={Logo} alt="Logo" />
         </Link>
 
         {/* ............Nav-Link............*/}
-        <ul className={`nav-links ${isNavLinkShow ? 'nav-link-show' : 'nav-link-hide'}`}>
-          {
-            navLinks.map(({name, path}, index) => {
-              return(
-                <li key={index}>
-                  <NavLink to={path} className={({isActive}) => 
-                isActive ? 'active' : ''}>{name}</NavLink>
-                </li>
-              )
-            })
-          }
+        <ul
+          className={`nav-links ${
+            isNavLinkShow ? "nav-link-show" : "nav-link-hide"
+          }`}
+        >
+          {navLinks.map(({ name, path }, index) => {
+            return (
+              <li key={index}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => (isActive ? "active" : "")}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
 
         {/* ............Nav-Right............*/}
         <div className="nav-right">
-          {
-            navRight.managements.map((item, index) => {
-              return (
-                <Link key={index} 
-                // target="_blank" 
-                className="management-icons" to={item.link}>
-                  <item.icon/>
-                </Link>
-              )
-            })
-          }
-          <button className="menu-button" onClick={() => setIsNavLinkShow(!isNavLinkShow)}>
-            {
-              isNavLinkShow ? <GrClose /> : 
-              <VscMenu />
-            }
-
+          {navRight.managements.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                // target="_blank"
+                className="management-icons"
+                to={item.link}
+              >
+                <item.icon />
+              </Link>
+            );
+          })}
+          <button
+            className="menu-button"
+            onClick={() => setIsNavLinkShow(!isNavLinkShow)}
+          >
+            {isNavLinkShow ? <GrClose /> : <VscMenu />}
           </button>
         </div>
-
       </div>
     </nav>
-  )
+  );
 }
